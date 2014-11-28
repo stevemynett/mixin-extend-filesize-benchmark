dcdf34277a716412c1cc9fae8467c7330dd8e05b
Steve Mynett <steve@mynettworks.com>
2014-11-27 22:41:46 -0800

    Refactored the less build tasks to handle multiple files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,36 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      less: {
-        extend: {
-          src: 'less/extend.less',
-          dest: 'css/extend.css'
-        },
-        mixin: {
-          src: 'less/mixin.less',
-          dest: 'css/mixin.css'
-        }
-    }
+    less: {
+      extend: {
+        files: [
+          {
+            expand: true,
+            cwd: 'less/extends/',
+            src: ['*.less'],
+            dest: 'css/',
+            ext: '.css'
+          }
+        ]
+      },
+      mixins: {
+        files: [
+          {
+            expand: true,
+            cwd: 'less/mixins/',
+            src: ['*.less'],
+            dest: 'css/',
+            ext: '.css'
+          }
+        ]
+      }
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
-  grunt.registerTask('default', ['less:extend', 'less:mixin']);
+  grunt.registerTask('default', ['less:extend', 'less:mixins']);
 
 };
